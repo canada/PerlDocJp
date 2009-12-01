0e4d837b9894ab819475f1c27eadc7afa474b7b8
lestrrat <lestrrat+github@gmail.com>
2009-12-01 12:44:37 +0900

    remove junk

--- lib/PerlDocJp/Controller/Root.pm
+++ lib/PerlDocJp/Controller/Root.pm
@@ -1,5 +1,4 @@
 package PerlDocJp::Controller::Root;
-
 use strict;
 use warnings;
 use parent 'Catalyst::Controller';
@@ -7,31 +6,7 @@ use YAML::Syck;
 use Encode;
 use Encode::Guess qw/eucjp utf8 shiftjis 7bit-jis/;
 
-#
-# Sets the actions in this controller to be registered with no prefix
-# so they function identically to actions created in MyApp.pm
-#
 __PACKAGE__->config->{namespace} = '';
-
-=head1 NAME
-
-PerlDocJp::Controller::Root - Root Controller for PerlDocJp
-
-=head1 DESCRIPTION
-
-[enter your description here]
-
-=head1 METHODS
-
-=cut
-
-=head2 index
-
-=cut
-
-
-sub begin :Private {
-}
 
 sub index :Path :Args(0) {
     my ( $self, $c ) = @_;
@@ -154,23 +129,6 @@ sub default :Path {
     $c->response->status(404);
 }
 
-=head2 end
-
-Attempt to render a view, if needed.
-
-=cut
-
 sub end : ActionClass('RenderView') {}
-
-=head1 AUTHOR
-
-,,,
-
-=head1 LICENSE
-
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
 
 1;
